c33b1f2cbb5b5e4da66cb186aaa1223e85aaf376
Arthur Pavlov <arthur.pavlovs@mail.utoronto.ca>
2018-02-03 23:33:20 -0500

    adding src files

---
+++ mainPage.js
@@ -0,0 +1,83 @@
+function getMeta(url, callback) {
+    var img = new Image();
+    img.src = url;
+    console.log(url);
+    img.onload = function() { callback(this.width, this.height); }
+}
+
+function insertImage(url){
+	getMeta(url, function(width, height){
+		var htmlString = $('body').html().toString();
+		if(htmlString.indexOf(url)== -1){
+			var src = document.getElementById("images-container");
+   			var div = document.createElement("div");
+   			div.className = "slide fade";
+   			var img = document.createElement("img");
+    		img.src = url;
+    		div.appendChild(img);
+    		src.appendChild(div);	
+		}
+	});
+}
+
+var timer;
+var init = 0;
+function clearContainer(){
+	if (init){
+		clearTimeout(timer);
+		init = 0;
+	}
+	$("#images-container").empty();
+}
+
+function getPageImages(pageurl){
+	clearContainer();
+	var root_url= 'https://www.instagram.com/';
+	var message = {link: root_url.concat(pageurl)};
+	$.ajax({
+ 		url: '/',
+ 		datatype: "json",
+ 		type: "POST",
+ 		data: message,
+ 		success: function(response){
+  			if(response.success[0] == 'invalid_account'){
+  				alert('invalid account');
+  			}
+  			else{
+  				var i = 0;
+  				while(response.success[i] != undefined){
+  					insertImage(response.success[i]);
+  					i++;
+  				}
+  			}
+ 		}
+	});
+}
+
+function init_ss(){
+	if(!init){
+		init = 1;
+		startSlideshow();
+	}
+}
+
+var slideIndex = 0;
+function startSlideshow() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+       	slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length){
+    	slideIndex = 1
+    }    
+    slides[slideIndex-1].style.display = "block";  
+    timer = setTimeout(startSlideshow, 5000); // Change image every 5 seconds
+}
+
+function getImages(){
+	if(document.getElementById("link").value != ''){
+		getPageImages(document.getElementById("link").value);
+	}
+}
---
+++ server.js
@@ -0,0 +1,90 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var app     = express();
+var bodyParser = require('body-parser');
+var Nightmare = require('nightmare');
+const cheerio = require('cheerio');       
+
+
+app.use(express.static(__dirname));
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+app.use(bodyParser.json());      
+app.use(bodyParser.urlencoded({   
+    extended: true
+}));
+
+
+app.get('/', function(req, res){
+    var errorMsgs = { 'errors': {} };
+    res.render('mainPage', errorMsgs);
+});
+
+
+app.post('/', function(req, res){
+    var userInput = JSON.parse(JSON.stringify(req.body));
+    var url = userInput.link;
+    // request(url, function(error, response, html){
+    request(url, function(error, response, html){
+        const $ = cheerio.load(html);
+        if($('.error-container').html() != null){
+            res.json({success: ['invalid_account']});
+        }   
+    });    
+
+         var urls = [];
+       
+            var nightmare = Nightmare(
+            { show: true });
+            nightmare.goto(url)
+                .wait('body')
+                .click('._1cr2e ')
+                .scrollTo(6000, 0).wait(200)
+                .scrollTo(6000, 0).wait(200)
+                .scrollTo(6000, 0).wait(200)
+                .scrollTo(10000, 0).wait(200)
+                .scrollTo(10000, 0).wait(200)
+                .scrollTo(20000, 0).wait(200)
+                .scrollTo(20000, 0).wait(200)
+                .scrollTo(20000, 0).wait(200)
+                .scrollTo(50000, 0).wait(200)
+                .scrollTo(50000, 0).wait(200)
+                .scrollTo(50000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .scrollTo(100000, 0).wait(200)
+                .evaluate(() => {
+                    return document.querySelector('body').innerHTML;
+                })
+                .end()
+                .then((result) => {
+                    // console.log(result);
+                    rex = /https:\/\/instagram\.[\w_\-\/\.]*\.jpg 640w|{"src":"https:\/\/instagram\.[\w_\-\/\.]*\.jpg","config_width":640,"config_height":640}/gi
+                    while (m = rex.exec(result)) {
+                        // console.log(m[0]);
+                        rex2 = /https:\/\/instagram\.[\w_\-\/\.]*\.jpg/gi
+                        while (m2 = rex2.exec(m[0])) {
+                            // console.log(m2[0]);
+                            urls.push(m2[0]);  
+                        }
+                    }
+                    res.json({success: urls});
+                })
+
+});
+
+
+app.listen(process.env.PORT || 80);
+
+console.log('Listening on port 80');
+
+exports = module.exports = app;
